48dfd0549cd2025fb62c76b27912c9fcc2b4038c
Carlos Cardona <cargicar@gmail.com>
2022-11-03 15:55:09 -0500

    Added pydicom

--- core/settings.py
+++ core/settings.py
@@ -31,9 +31,9 @@ os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-f7c5.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['web-production-f7c5.up.railway.app']
 
 # Application definition
 
