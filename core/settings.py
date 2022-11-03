f4fd23d10c99d3b542c3494a15988adcca144220
Carlos Cardona <cargicar@gmail.com>
2022-11-03 16:03:23 -0500

    Added pydicom

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-f7c5.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['web-production-f7c5.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['http://web-production-f7c5.up.railway.app', 'https://web-production-f7c5.up.railway.app']
 
 # Application definition
 
