8c0b1f6593d1da7688e0221050be1c8bbe15e12b
Carlos Cardona <cargicar@gmail.com>
2022-11-05 13:37:50 -0500

    Debug=False

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-2cb9.up.railway.app','urchin-app-fei4q.ondigitalocean.app', 'localhost']
 
